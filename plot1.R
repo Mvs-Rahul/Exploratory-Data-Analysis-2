0889217829ff90c10a824b0d0b0a6a02e32f6e53
Mvs-Rahul <112823898+Mvs-Rahul@users.noreply.github.com>
2022-10-28 20:41:58 +0530

    Course Project 2

--- plot1.R
+++ plot1.R
@@ -13,3 +13,5 @@ NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
 totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
 
 barplot(totalNEI[, Emissions], names = totalNEI[, year], xlab = "Years", ylab = "Emissions", main = "Emissions over the Years")
+
+dev.off()
