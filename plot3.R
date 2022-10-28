19c8d444843be752c740709794cc5c2d4262547c
Mvs-Rahul <112823898+Mvs-Rahul@users.noreply.github.com>
2022-10-28 21:14:11 +0530

    Course Project 2

--- plot3.R
+++ plot3.R
@@ -3,8 +3,7 @@ library("ggplot2")
 
 setwd("~/Desktop/4_Exploratory_Data_Analysis_project2")
 path <- getwd()
-download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
-              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destfile = paste(path, "dataFiles.zip", sep = "/"))
 unzip(zipfile = "dataFiles.zip")
 
 NEI <- data.table::as.data.table(x = readRDS("summarySCC_PM25.rds"))
